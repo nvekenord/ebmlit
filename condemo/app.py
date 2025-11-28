cec73ef1433f82a0b8fed97ac0972cfc233881d9
kenord <kenord@nve.no>
2025-11-28 13:12:23 +0100

    Add formatting and meta data

--- condemo/app.py
+++ condemo/app.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from ebm import extractors
 from loguru import logger
+
+from ebm.__version__ import version as ebm_version
 from ebm.model.data_classes import YearRange
 from ebm.model.database_manager import DatabaseManager
 from ebm.s_curve import calculate_s_curves
@@ -11,31 +13,32 @@ from ebm.model import heating_systems_parameter as h_s_param
 import streamlit as st
 
 years = YearRange(2020, 2050)
-database_manager = DatabaseManager()
 
-scurve_parameters = database_manager.get_scurve_params() # 📍
 
-area_parameters = database_manager.get_area_parameters() # 📍
-area_parameters['year'] = years.start
+def load_demolition_construction():
+    database_manager = DatabaseManager()
+    scurve_parameters = database_manager.get_scurve_params()  # 📍
+    area_parameters = database_manager.get_area_parameters()  # 📍
+    area_parameters['year'] = years.start
+    building_code_parameters = database_manager.file_handler.get_building_code()  # 📍
+    s_curves_by_condition = calculate_s_curves(scurve_parameters, building_code_parameters, years)  # 📌
+    area_forecast = extractors.extract_area_forecast(years, s_curves_by_condition, building_code_parameters,
+                                                     area_parameters, database_manager)  # 📍
+    energy_need_kwh_m2 = extractors.extract_energy_need(years, database_manager)  # 📍
+    heating_systems_projection = extractors.extract_heating_systems_forecast(years, database_manager)  # 📍
 
-building_code_parameters = database_manager.file_handler.get_building_code() # 📍
+    total_energy_need = e_n.transform_total_energy_need(energy_need_kwh_m2, area_forecast)  # 📌
+    heating_systems_parameter = h_s_param.heating_systems_parameter_from_projection(heating_systems_projection)  # 📌
+    energy_use_kwh = e_u.building_group_energy_use_kwh(heating_systems_parameter, total_energy_need)  # 📌
+    area_change = a_f.transform_area_forecast_to_area_change(area_forecast=area_forecast,
+                                                             building_code_parameters=building_code_parameters)
+    demolition_construction_long = a_f.transform_demolition_construction(energy_use_kwh, area_change)
+    demolition_construction_long = demolition_construction_long.set_index(['building_category', 'demolition_construction', 'year']).sort_index()
+    return demolition_construction_long
 
-s_curves_by_condition = calculate_s_curves(scurve_parameters, building_code_parameters, years) # 📌
-area_forecast = extractors.extract_area_forecast(years, s_curves_by_condition, building_code_parameters, area_parameters, database_manager) # 📍
-energy_need_kwh_m2 = extractors.extract_energy_need(years, database_manager) # 📍
-heating_systems_projection = extractors.extract_heating_systems_forecast(years, database_manager) # 📍
-energy_use_holiday_homes = extractors.extract_energy_use_holiday_homes(database_manager) # 📍
 
-total_energy_need = e_n.transform_total_energy_need(energy_need_kwh_m2, area_forecast)  # 📌
-heating_systems_parameter = h_s_param.heating_systems_parameter_from_projection(heating_systems_projection) # 📌
-energy_use_kwh = e_u.building_group_energy_use_kwh(heating_systems_parameter, total_energy_need) # 📌
 
-area_change = a_f.transform_area_forecast_to_area_change(area_forecast=area_forecast, building_code_parameters=building_code_parameters)
-
-demolition_construction_long = a_f.transform_demolition_construction(energy_use_kwh, area_change)
-
-df = demolition_construction_long.set_index(['building_category', 'demolition_construction', 'year']).sort_index()
-
+df = load_demolition_construction()
 
 available_building_categories = list(df.index.get_level_values(level='building_category').unique())
 available_building_groups = ['Residential', 'Non-residential', 'All']
@@ -45,20 +48,26 @@ available_units = ['m2', 'gwh']
 df['building_group'] = 'Non-residential'
 df.loc[(['apartment_block', 'house'], slice(None), slice(None)), 'building_group'] = 'Residential'
 
+page_title = 'EBM demolition and construction'
+
+st.set_page_config(page_title=page_title)
+st.markdown(f'# {page_title}')
+
+
 building_category = st.selectbox('building_category', available_building_groups + available_building_categories)
 
 
 demolition_construction = st.selectbox('area type', available_area_types)
 unit = st.selectbox('unit', available_units)
 years = st.multiselect('Year', years.year_range, placeholder=f'{years.start}-{years.end}')
+st.markdown(f'## {building_category} {demolition_construction if not demolition_construction=="Both" else ""} {unit}')
+st.badge(f'ebm {ebm_version}')
 
 df = pd.pivot_table(df, values=['m2', 'gwh'], index=['building_category', 'building_group', 'building_code', 'year'],
                     columns=['demolition_construction'], aggfunc="sum")
 
 df=df.reset_index()
 df.columns = ['building_category', 'building_group', 'building_code', 'year', 'construction gwh', 'demolition gwh', 'construction m2', 'demolition m2']
-
-st.dataframe(df[['building_category', 'building_group']])
 
 if building_category in ('Residential', 'Non-residential'):
     df['building_category'] = df['building_group']
@@ -76,3 +85,19 @@ if years:
 columns = [f'construction {unit}', f'demolition {unit}'] if demolition_construction == 'both' else f'{demolition_construction} {unit}'
 
 st.bar_chart(df, x='year', y=columns)
+
+repo_url = 'https://github.com/nvekenord/ebmlit'
+github_icon='https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png'
+
+
+
+
+
+st.markdown(
+    f"""
+    <a href="{repo_url}" target="_blank" style="text-decoration:none;">
+        <img src="{github_icon}" width="25" style="vertical-align:middle; margin-right:8px;">ebmlit.condemo</a>
+    """,
+    unsafe_allow_html=True)
+
+
